7ac72f0d99eb2a1fb6b776e2dcd489e4f0edbac0
Changjin Lee <67718024+JasonLee-cp@users.noreply.github.com>
2020-08-31 15:26:55 +0900

    #15656

---
+++ BOJ/Backtracking/[BOJ] 15656번 N과M (7).cpp
@@ -0,0 +1,40 @@
+
+#include <iostream>
+#include <vector>
+#include <unordered_set>
+#include <algorithm>
+using namespace std;
+int n, m;
+vector<int>arr;
+vector<int>res;
+unordered_set<int>visited;
+void backtracking(int k) {
+	if (k == m) {
+		for (int i = 0; i < m; i++)
+			cout << res[i] << " ";
+		cout << "\n";
+		return;
+	}
+
+	for (int i = 0; i < n; i++) {
+	//	if (visited.count(arr[i])) continue;
+		res[k] = arr[i];
+		//visited.insert(arr[i]);
+		backtracking(k + 1);
+		//visited.erase(arr[i]);
+	}
+
+}
+
+
+int main() {
+	cin >> n >> m;
+	for (int i = 0; i < n; i++) {
+		int num; cin >> num;
+		arr.push_back(num);
+	}
+	res = vector<int>(m);
+	sort(arr.begin(), arr.end());
+	backtracking(0);
+
+}
