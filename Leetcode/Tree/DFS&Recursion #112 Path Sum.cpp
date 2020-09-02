dea59998abaedcf16043d9113a665c2ac8e16f08
Changjin Lee <67718024+JasonLee-cp@users.noreply.github.com>
2020-09-02 18:25:45 +0900

    #112

---
+++ Leetcode/Tree/DFS&Recursion #112 Path Sum.cpp
@@ -0,0 +1,33 @@
+
+class Solution {
+public:
+     bool hasPathSum(TreeNode* root, int sum) {
+        if(!root)return false;
+         if(!root->left && !root->right && sum==root->val) return true;
+         return hasPathSum(root->left, sum-root->val) ||  hasPathSum(root->right, sum-root->val);
+
+    }
+/*  bool istrue=false;
+    int global_sum;
+   void pathsum(TreeNode* root, int sum){
+       if(!root)return;
+       sum+=root->val;
+       if(!root->left && !root->right){
+           if(sum==global_sum){
+               istrue=true;
+               return;
+           }
+           else return;
+       }
+       pathsum(root->left,sum);
+       pathsum(root->right,sum);
+       
+       
+   }
+    bool hasPathSum(TreeNode* root, int sum) {
+        global_sum=sum;
+        pathsum(root,0);
+        return istrue;
+
+    }*/
+};
