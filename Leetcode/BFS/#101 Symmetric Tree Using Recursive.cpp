eef0d3b2143fa600f319a922156b704a2a01890c
Changjin Lee <67718024+JasonLee-cp@users.noreply.github.com>
2020-09-18 01:02:47 +0900

    #101 Recursive

---
+++ Leetcode/BFS/#101 Symmetric Tree Using Recursive.cpp
@@ -0,0 +1,14 @@
+class Solution {
+public:
+    bool isSymmetric(TreeNode* root) {
+       if(root==NULL)return true;
+        return helper(root->left, root->right);
+    }
+    
+    bool helper(TreeNode* r1, TreeNode* r2){
+        if(r1==NULL && r2==NULL)return true;
+        if(r1==NULL || r2==NULL)return false;
+        if(r1->val != r2->val)return false;
+        return helper(r1->left,r2->right) && helper(r1->right,r2->left);
+    }
+};
