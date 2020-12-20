2f944828411f1305474e68424f248c2eff196f6a
Jason Lee <changjin9792@gmail.com>
2020-12-21 00:47:38 +0900

    #416

--- Leetcode/Dynamic Programming/#416. Partition Equal Subset Sum.cpp
+++ Leetcode/Dynamic Programming/#416. Partition Equal Subset Sum.cpp
@@ -1,3 +1,4 @@
+//Bottom Up
 class Solution {
 public:
     int target;
@@ -24,5 +25,34 @@ public:
         
         
        
+    }
+};
+
+//TopDown (2^n)
+class Solution {
+public:
+    vector<vector<int>>dp;
+  
+    bool topdown(vector<int>&nums, int target, int i){
+        if(target<0 || i<0) return false;
+        if(target==0) return true;
+        if(dp[i][target]!=-1) return dp[i][target];
+        
+        if(nums[i]>target) dp[i][target]=topdown(nums,target,i-1);
+        else{
+            dp[i][target]=topdown(nums,target-nums[i],i-1) || topdown(nums,target,i-1);
+        }
+     
+        return dp[i][target];
+        
+    }
+    bool canPartition(vector<int>& nums) {
+        int n = nums.size();
+        int total_sum = accumulate(nums.begin(), nums.end(), 0);
+        if(total_sum%2!=0) return false;
+        total_sum/=2; //this is our target
+         dp.clear();
+         dp.resize(n,vector<int>(total_sum+1,-1));
+        return topdown(nums,total_sum,n-1);
     }
 };
