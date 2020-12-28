6317060c134e24898591ec88560fb2e139a3795b
Jason Lee <changjin9792@gmail.com>
2020-12-28 19:49:44 +0900

    #787

--- Leetcode/Dynamic Programming/#787. Cheapest Flights Within K Stops.cpp
+++ Leetcode/Dynamic Programming/#787. Cheapest Flights Within K Stops.cpp
@@ -1,3 +1,4 @@
+//Bellman Ford
 class Solution {
 public:
     int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
@@ -15,5 +16,37 @@ public:
             dist=temp;
         }
         return dist[dst]==INF?-1:dist[dst];
+    }
+};
+
+//Dijkstra & Priority Queue - modified(다시)
+class Solution {
+public:
+    int findCheapestPrice(int n, vector<vector<int>>& flights, int src, int dst, int K) {
+        vector<vector<pair<int,int>>>adj(n);
+        for(auto f: flights){
+            adj[f[0]].push_back({f[1],f[2]});
+        }
+        
+        priority_queue<vector<int>, vector<vector<int>>, greater<vector<int>>>pq;
+        pq.push({0,src,K+1});
+        
+        while(!pq.empty()){
+            auto cur = pq.top(); pq.pop();
+            int cost = cur[0];
+            int node = cur[1];
+            int cnt = cur[2];
+          
+            
+            if(node==dst) return cost;
+            
+            if(cnt>0){
+                for(auto next: adj[node]){
+                    pq.push({cost+next.second,next.first,cnt-1});
+                }
+            }
+            
+        }
+          return -1; 
     }
 };
