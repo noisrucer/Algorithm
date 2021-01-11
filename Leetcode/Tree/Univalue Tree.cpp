d8d18f2a3de02721044b6897a9a589a7afd355a8
Jason Lee <changjin9792@gmail.com>
2021-01-12 02:36:02 +0800

    univalue tree

---
+++ Leetcode/Tree/Univalue Tree.cpp
@@ -0,0 +1,17 @@
+/**
+ * class Tree {
+ *     public:
+ *         int val;
+ *         Tree *left;
+ *         Tree *right;
+ * };
+ */
+bool dfs(Tree* root, int val){
+    if(!root) return true;
+    if(root->val != val) return false;
+    return dfs(root->left,val) && dfs(root->right,val);
+}
+bool solve(Tree* root) {
+    if(!root) return true;
+    return dfs(root,root->val);
+}
