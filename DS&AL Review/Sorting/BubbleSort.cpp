647da1e44af14c9fc90b3d94f4a677296d2747ab
Jason Lee <changjin9792@gmail.com>
2021-01-26 00:20:58 +0800

    optimized bubble sort

--- DS&AL Review/Sorting/BubbleSort.cpp
+++ DS&AL Review/Sorting/BubbleSort.cpp
@@ -4,10 +4,14 @@ using namespace std;
 
 void bubbleSort(vector<int>&arr){
     int n = arr.size();
+    bool flag;
     for(int i=0; i<n; i++){
+        flag = false;
         for(int j=0; j<n-1-i; j++){
             if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
+            flag = true;
         }
+        if(!flag) return;
     }
 }
 int main(){
