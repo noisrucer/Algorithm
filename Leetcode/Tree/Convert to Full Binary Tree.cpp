65fbe37ee34b7b1b8c7d73f569096f91849a83f3
Jason Lee <changjin9792@gmail.com>
2021-01-27 13:28:49 +0800

    convert to full bt

--- Leetcode/Tree/Convert to Full Binary Tree.cpp
+++ Leetcode/Tree/Convert to Full Binary Tree.cpp
@@ -23,3 +23,20 @@ Tree* solve(Tree* root) {
     return dfs(root);
     
 }
+
+//2
+Tree* dfs(Tree* root){
+    if(!root) return NULL;
+    if(root->left==root->right) return root;
+    root->left = dfs(root->left);
+    root->right = dfs(root->right);
+    if(root->left && root->right) return root; //only one child
+
+    if(root->left) return root->left;
+    else return root->right;
+    
+} 
+Tree* solve(Tree* root) {
+    return dfs(root);
+    
+}
