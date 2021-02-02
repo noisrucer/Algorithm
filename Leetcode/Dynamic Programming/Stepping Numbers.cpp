8066db31db32b5f4e49e8f346a8d899f05f66b14
Jason Lee <changjin9792@gmail.com>
2021-02-02 22:13:19 +0800

    stepping numbers

---
+++ Leetcode/Dynamic Programming/Stepping Numbers.cpp
@@ -0,0 +1,24 @@
+vector<vector<int>>dp;
+const int MODE = pow(10,9)+7;
+
+int topdown(int n, int i){
+    if(n==1){
+        if(i<=0 || i>9) return 0;
+        else return 1;
+    }
+    if(dp[n][i]!=-1) return dp[n][i];
+    dp[n][i] = (i-1>=0 ? topdown(n-1,i-1)%MODE : 0) + (i+1<=9 ? topdown(n-1,i+1)%MODE : 0);
+    return dp[n][i]%MODE;
+}
+
+int solve(int n) {
+    if(n==1) return 10;
+    dp.clear();
+    dp.resize(n+1,vector<int>(10,-1));
+    long long res=0;
+    for(int i=0; i<=9; i++){
+        res+=topdown(n,i)%MODE;
+        res%=MODE;
+    }
+    return res;
+}
