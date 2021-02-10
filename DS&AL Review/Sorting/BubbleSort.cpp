637ffd8c9fcf193a1e5e5674431a277c6645065b
Jason Lee <changjin9792@gmail.com>
2021-02-11 00:43:40 +0800

    array

--- DS&AL Review/Sorting/BubbleSort.cpp
+++ DS&AL Review/Sorting/BubbleSort.cpp
@@ -8,8 +8,10 @@ void bubbleSort(vector<int>&arr){
     for(int i=0; i<n-1; i++){
         flag = false;
         for(int j=0; j<n-1-i; j++){
-            if(arr[j]>arr[j+1]) swap(arr[j],arr[j+1]);
-            flag = true;
+            if(arr[j]>arr[j+1]){
+                swap(arr[j],arr[j+1]);
+                flag = true;
+            }
         }
         if(!flag) return;
     }
