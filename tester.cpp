3bd2e29bc714062aa6e8957e40533c0fc7f1876a
Jason Lee <changjin9792@gmail.com>
2021-02-14 22:45:44 +0800

    domino placement

--- tester.cpp
+++ tester.cpp
@@ -1,12 +1,14 @@
 #include <iostream>
 #include <vector>
 using namespace std;
-
+int solve(int n, int m) {
+    if(n==1) return (m+1)/2;
+    if(m==1) return (n+1)/2;
+    if(n%2==0 || m%2==0) return n*m/2;
+    return solve(n,m-1) + solve(n,1);
+}
 int main(){
-    vector<int>res;
-    res.reserve(2);
-    cout<<res.size();
-    res[0]=10;
-    res[1]=20;
-    for(auto i: res) cout<<i<<" ";
+   double x = 1.5;
+   cout<<int(x)<<endl;
+   cout<<(int)x<<endl;
 }
