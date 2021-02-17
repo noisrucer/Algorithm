2065f19da6719984249d45f67cd3876c932c9f08
Jason Lee <changjin9792@gmail.com>
2021-02-18 00:47:17 +0800

    anagram

---
+++ Leetcode/Array/Anagram Checks.cpp
@@ -0,0 +1,8 @@
+bool solve(string s0, string s1) {
+    unordered_map<char,int>mp;
+    for(char c: s0) mp[c]++;
+    for(char c: s1){
+        if(--mp[c]==0) mp.erase(c);
+    }
+    return mp.size()==0;
+}
