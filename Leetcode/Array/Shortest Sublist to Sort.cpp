ae2b8e25783c33d2d81b0ec6cd1b7c1ec1ce6676
Jason Lee <changjin9792@gmail.com>
2021-02-19 03:10:21 +0800

    Shortest Sublist to Sort

---
+++ Leetcode/Array/Shortest Sublist to Sort.cpp
@@ -0,0 +1,12 @@
+int solve(vector<int>& nums) {
+    int n = nums.size();
+    vector<int> sorted(nums);
+    sort(sorted.begin(), sorted.end());
+    vector<int>res;
+    
+    for(int i=0; i<n; i++){
+        if(sorted[i]!=nums[i]) res.push_back(i);
+    }
+    if(res.size()==0) return 0;
+    return res[res.size()-1]-res[0] + 1;
+}
