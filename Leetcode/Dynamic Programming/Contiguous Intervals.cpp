bdd26659bc8e3ae664555036d02a2017050199c4
Jason Lee <changjin9792@gmail.com>
2021-02-28 00:24:47 +0800

    contiguous intervals

---
+++ Leetcode/Dynamic Programming/Contiguous Intervals.cpp
@@ -0,0 +1,17 @@
+vector<vector<int>> solve(vector<int>& nums) {
+    vector<vector<int>>res;
+    sort(nums.begin(), nums.end());
+    int n = nums.size();
+    if(n==0) return {};
+
+    nums.push_back(nums[n++-1]+2);
+    int left = nums[0];
+    
+    for(int i=1; i<n; i++){
+        if(nums[i]!=nums[i-1]+1){
+            res.push_back({left,nums[i-1]});
+            left = nums[i];
+        }
+    }
+    return res;
+}
