f4311c6f61c145b4b6247d7dd7047b4432da130d
Jason Lee <changjin9792@gmail.com>
2021-03-01 17:38:16 +0800

    Increasing Digits

---
+++ Leetcode/Dynamic Programming/Increasing Digits.cpp
@@ -0,0 +1,24 @@
+vector<vector<int>>dp;
+int topdown(int n, int i){
+    if(n==1){
+        if(i==0) return 0;
+        else return 1;
+    }
+    if(dp[n][i]!=-1) return dp[n][i];
+    
+    int temp = 0;
+    for(int k=0; k<i; k++){
+        temp += topdown(n-1,k);
+    }
+    return dp[n][i] = temp;
+}
+
+int solve(int n) {
+    dp.clear();
+    dp.resize(n+1,vector<int>(10,-1));
+    int res = 0;
+    for(int i=0; i<=9; i++){
+        res += topdown(n,i);
+    }
+    return res;
+}
