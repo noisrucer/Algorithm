17b861e7a2c1a83b2c76e29426abdb8ba9d5e704
Jason Lee <changjin9792@gmail.com>
2021-03-03 01:55:29 +0800

    Large to Small Sort

---
+++ Leetcode/Array/Large to Small Sort.cpp
@@ -0,0 +1,13 @@
+vector<int> solve(vector<int>& nums) {
+    int n = nums.size();
+    vector<int>res;
+    sort(nums.begin(), nums.end());
+    int i = 0, j = n-1, flag = 0;
+    while(i<=j){
+        if(flag & 1) res.push_back(nums[i++]);
+        else res.push_back(nums[j--]);
+        flag++;
+    }
+    return res;
+}
+
