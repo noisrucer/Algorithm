4ee95deed92e5762ff8af649819305c5e27bbaa3
Jason Lee <changjin9792@gmail.com>
2021-03-02 22:32:43 +0800

    Make a Palindrome by Inserting Characeteres

--- Leetcode/Dynamic Programming/Make a Palindrome by Inserting Characters.cpp
+++ Leetcode/Dynamic Programming/Make a Palindrome by Inserting Characters.cpp
@@ -1,3 +1,4 @@
+//Top Down
 vector<vector<int>>dp;
 int n;
 int topdown(string &s, int i, int j){
@@ -15,5 +16,25 @@ int solve(string s) {
    dp.clear();
    dp.resize(n,vector<int>(n,-1));
    return topdown(s,0,n-1);
+    
+}
+
+//Bottom Up
+int solve(string s) {
+   int n = s.length();
+   vector<vector<int>>dp(n,vector<int>(n,0));
+   for(int i=0; i<n-1; i++){
+      dp[i][i+1] = s[i]!=s[i+1];
+   }
+
+   for(int len=3; len<=n; len++){
+      for(int i=0; i<=n-len; i++){
+         int j = i+len-1;
+         if(s[i]==s[j]) dp[i][j] = dp[i+1][j-1];
+         else dp[i][j] = min({1+dp[i+1][j],1+dp[i][j-1],2+dp[i+1][j-1]});
+      }
+   }
+
+   return dp[0][n-1]; 
     
 }
