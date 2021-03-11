6a99b1a7401eb9c51ac46e314370b6e3b49a2fd2
Jason Lee <changjin9792@gmail.com>
2021-03-12 02:06:03 +0800

    Count Square Submatrices

---
+++ Leetcode/Dynamic Programming/Count Square Submatrices.cpp
@@ -0,0 +1,13 @@
+int solve(vector<vector<int>>& matrix) {
+    int n = matrix.size(), m = matrix[0].size();
+    vector<vector<int>>dp(n+1,vector<int>(m+1,0));
+    int res = 0;
+    for(int i=1; i<=n; i++){
+        for(int j=1; j<=m; j++){
+            if(matrix[i-1][j-1]==0) continue;
+            dp[i][j] = 1 + min({dp[i-1][j],dp[i][j-1],dp[i-1][j-1]});
+            res += dp[i][j];
+        }
+    }
+    return res;
+}
