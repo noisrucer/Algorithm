28f3d8900436a94705fa6217392556d8857f9b5a
Jason Lee <changjin9792@gmail.com>
2021-06-17 01:17:54 +0800

    Cut Rods for Profit

---
+++ Leetcode/Dynamic Programming/Cut Rods for Profit.cpp
@@ -0,0 +1,31 @@
+vector<vector<int>>dp;
+int unit_profit;
+int cut_cost;
+
+// dp[i][j] = 0-i까지 rods들을 이용해서 j length인 rod만을 팔아서 얻을수 있는 max profit
+int topdown(vector<int>&rods, int i, int j){ 
+    if(i < 0) return 0;
+    if(rods[i] < j) return dp[i][j] = 0;
+    if(rods[i]==j) dp[i][j] = rods[i] * unit_profit + topdown(rods,i-1,j);
+    if(dp[i][j] != INT_MIN) return dp[i][j];
+    
+    dp[i][j] = max(topdown(rods,i-1,j),((rods[i]/j) * j * unit_profit - ((rods[i]-1)/j)*cut_cost) + topdown(rods, i-1, j));
+    return dp[i][j];
+}
+
+int solve(vector<int>& rods, int p, int c) {
+    if(rods.size()==0) return 0;
+    sort(rods.begin(), rods.end());
+    int n = rods.size();
+    
+    dp = {};
+    dp.resize(n,vector<int>(rods[n-1]+1,INT_MIN));
+    unit_profit = p;
+    cut_cost = c;
+
+    int res = 0;
+    for(int j=1; j<=rods[n-1]; j++)
+        res = max(res, topdown(rods,n-1, j));
+    
+    return res;
+}
