a8c8a8aa2c05f573bf9d2cae2ac8012bb8ec5629
Jason Lee <changjin9792@gmail.com>
2021-06-17 22:56:53 +0800

    String Construction

---
+++ Leetcode/Dynamic Programming/String Construction.cpp
@@ -0,0 +1,23 @@
+vector<vector<vector<int>>>dp;
+int n;
+
+int topdown(vector<string>& strings, int i, int p, int q){
+    if(i<0) return 0;
+    if(dp[i][p][q]!=INT_MIN) return dp[i][p][q];
+    int num_A = count(strings[i].begin(), strings[i].end(),'A');
+    int len = strings[i].length();
+
+    if(p>=num_A && q>=len-num_A){
+        dp[i][p][q] = max(1+topdown(strings,i-1,p-num_A,q-(len-num_A)), topdown(strings,i-1,p,q));
+    }else{
+        dp[i][p][q] = topdown(strings,i-1,p,q);
+    }
+    return dp[i][p][q];
+}
+
+int solve(vector<string>& strings, int a, int b) {
+    n = strings.size();
+    dp={};
+    dp.resize(n,vector<vector<int>>(a+1,vector<int>(b+1,INT_MIN)));
+    return topdown(strings,n-1,a,b);
+}
