38ca43546cb535d8cf4639ab120e8df1212c100c
Jason Lee <changjin9792@gmail.com>
2021-06-17 23:06:28 +0800

    String Construction

--- Leetcode/Dynamic Programming/String Construction.cpp
+++ Leetcode/Dynamic Programming/String Construction.cpp
@@ -1,3 +1,4 @@
+// TOPDOWN
 vector<vector<vector<int>>>dp;
 int n;
 
@@ -20,4 +21,24 @@ int solve(vector<string>& strings, int a, int b) {
     dp={};
     dp.resize(n,vector<vector<int>>(a+1,vector<int>(b+1,INT_MIN)));
     return topdown(strings,n-1,a,b);
+}
+
+//BOTTOM UP
+
+//Bottom Up
+int solve(vector<string>& strings, int a, int b) {
+    int n = strings.size(), countA, len;
+    vector<vector<vector<int>>>dp(n+1,vector<vector<int>>(a+1,vector<int>(b+1,0)));
+    for(int i=1; i<=n; i++){
+        for(int p=0; p<=a; p++){
+            for(int q=0; q<=b; q++){
+                 countA = count(strings[i-1].begin(), strings[i-1].end(),'A');
+                 len = strings[i-1].length();
+                 if(p>=countA && q>=len-countA) dp[i][p][q] = max(1 + dp[i-1][p-countA][q-(len-countA)], dp[i-1][p][q]);
+                 else dp[i][p][q] = dp[i-1][p][q];
+            }
+        }
+    }
+
+    return dp[n][a][b];
 }
