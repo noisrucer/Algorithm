77d0ea449a70ee9aed44f3ce07ac427edbb879bd
Jason Lee <changjin9792@gmail.com>
2021-06-19 23:57:07 +0800

    Binary Tree to Linked List

--- Leetcode/DFS/Unlock Rooms.cpp
+++ Leetcode/DFS/Unlock Rooms.cpp
@@ -18,3 +18,31 @@ bool solve(vector<vector<int>>& rooms) {
     dfs(rooms,0);
     return cnt==n;
 }
+
+
+
+// Solution 2
+vector<int>visited;
+
+void dfs(vector<vector<int>>& rooms, int cur){
+    if(visited[cur]) return;
+    visited[cur] = true;
+
+    for(auto &neighbor: rooms[cur]){
+        dfs(rooms,neighbor);
+    }
+}
+
+bool solve(vector<vector<int>>& rooms) {
+    int n = rooms.size();
+    visited={}; 
+    visited.resize(n,false);
+    dfs(rooms,0);
+
+    for(auto &i: visited){
+        if(i==false) return false;
+    }
+    
+    return true;
+    
+}
