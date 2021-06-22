84d90b02a8182fdbe9a2103c536f3871f2b41a9e
Jason Lee <changjin9792@gmail.com>
2021-06-22 23:01:10 +0800

    Sort String by Flipping

---
+++ Leetcode/Dynamic Programming/Sort String by Flipping.cpp
@@ -0,0 +1,22 @@
+int solve(string s) {
+    int n = s.length();
+    if(n==0) return 0;
+    vector<int>leftX(n,0), rightY(n,0);
+
+    
+    leftX[0] = (s[0] == 'y');
+    rightY[n-1] = (s[n-1] == 'x');
+
+    for(int i=1; i<n; i++){
+        leftX[i] = leftX[i-1] + (s[i] == 'y');
+        rightY[n-1-i] = rightY[n-i] + (s[n-1-i] == 'x');
+    }
+    
+    int res = INT_MAX;
+    for(int i=0; i<n; i++){
+        res = min(res, leftX[i]+rightY[i+1]);
+    }
+
+    res = min({res,leftX[n-1],rightY[0]});
+    return res == INT_MAX ? 0 : res;
+}
