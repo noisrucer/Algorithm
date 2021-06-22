5bfc9db8060de09ba3c8cdda40635704c95f6215
Jason Lee <changjin9792@gmail.com>
2021-06-22 22:21:34 +0800

    Unidrectional Word SEarch

---
+++ Leetcode/String/Unidirectional Word Search.cpp
@@ -0,0 +1,20 @@
+bool solve(vector<vector<string>>& board, string word) {
+    int n = board.size(), m = board[0].size();
+    string temp_row="", temp_col="";
+    vector<string>col(m,"");
+
+    for(int i=0; i<n; i++){
+        for(int j=0; j<m; j++){
+            temp_row += board[i][j];
+            col[j] += board[i][j];
+        }
+        if(temp_row.find(word) != std::string::npos) return true;
+        temp_row = "";
+    }
+
+    for(auto &str: col){
+        if(str.find(word) != std::string::npos) return true;
+    }
+
+    return false;
+}
