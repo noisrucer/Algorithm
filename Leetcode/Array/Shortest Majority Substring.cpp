7e5bc14a6a7b461adfec961d3727e8246d459682
Jason Lee <changjin9792@gmail.com>
2021-07-05 23:29:51 +0800

    Shortest Majority Substring

---
+++ Leetcode/Array/Shortest Majority Substring.cpp
@@ -0,0 +1,14 @@
+int solve(string s) {
+    int n = s.length();
+    int res = INT_MAX, temp;
+    int count[26]{};
+    for(int i=0; i<26; i++) count[i] = -1;
+    
+    for(int i=0; i<n; i++){
+        temp = count[s[i]-'a'];
+        if(temp != -1 && 3 > i-temp) res = min(res,i-temp+1);
+        count[s[i]-'a'] = i;
+    }
+
+    return res == INT_MAX ? -1 : res;
+}
