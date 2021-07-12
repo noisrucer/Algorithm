955be4d77ec5334580268ba6e7c441a4a75a6bb6
Jason Lee <changjin9792@gmail.com>
2021-07-12 22:47:16 +0800

    Diagonal Tree Traversal

---
+++ Leetcode/Tree/Diagonal Tree Traversal.cpp
@@ -0,0 +1,15 @@
+vector<int>res;
+void helper(Tree* root, int level){
+    if(!root) return;
+    if(res.size() < level + 1) res.push_back(root->val);
+    else res[level] += root->val;
+
+    helper(root->left, level+1);
+    helper(root->right, level);
+}
+
+vector<int> solve(Tree* root) {
+    res.clear();    
+    helper(root,0);
+    return res;
+}
