0f137af7201c0e1007ba0d607e62cf80aad84c21
Jason Lee <changjin9792@gmail.com>
2021-07-14 17:53:40 +0800

    Validate Delivery Orrders

---
+++ Leetcode/String/Validate Delivery Orders.cpp
@@ -0,0 +1,19 @@
+bool solve(vector<string>& orders) {
+    unordered_set<string>pickups;
+    unordered_set<string>history;
+    string order_num;
+
+    for(string &order: orders){
+        order_num = order.substr(1);
+        if(order[0]=='P'){
+            if(history.count(order_num)) return false;
+            pickups.insert(order_num);
+        }else{
+            if(!pickups.count(order_num)) return false;
+            pickups.erase(order_num);
+        }
+        history.insert(order_num);
+    }
+
+    return pickups.size()==0;
+}
