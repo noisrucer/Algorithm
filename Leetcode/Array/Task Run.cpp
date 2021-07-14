e19a5dc23de30214200f96b15572d17c10c0d9b6
Jason Lee <changjin9792@gmail.com>
2021-07-14 23:05:17 +0800

    Task Run

--- Leetcode/Array/Task Run.cpp
+++ Leetcode/Array/Task Run.cpp
@@ -1,15 +1,15 @@
 int solve(vector<int>& nums, int k) {
     int n = nums.size();
-    unordered_map<int,int>mp;
+    unordered_map<int,int>timestamp;
     int offset = 0;
     k++;
 
     // i + offset refer to "actual index" after inserting "waits"
     for(int i=0; i<n; i++){
-        if(mp.count(nums[i]) && i + offset - mp[nums[i]] < k){
-            offset += (k - (i + offset - mp[nums[i]]));
+        if(timestamp.count(nums[i]) && i + offset - timestamp[nums[i]] < k){
+            offset += (k - (i + offset - timestamp[nums[i]]));
         }
-        mp[nums[i]] = i + offset;
+        timestamp[nums[i]] = i + offset;
     }
 
     return n + offset;
