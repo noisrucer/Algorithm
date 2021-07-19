7bcaa18353e66d849a36fb4edcd1efae050380a7
Jason Lee <changjin9792@gmail.com>
2021-07-19 15:54:27 +0800

    Consecutive Duplicates

---
+++ Leetcode/String/Consecutive Duplicates.cpp
@@ -0,0 +1,19 @@
+string solve(string s) {
+    int n = s.length();
+    s += (s[n-1]=='X'?'Y':'X');
+    // s = (s[0]=='X'?'Y':'X') + s;
+    n+=1;
+
+    int cnt = 1;
+    string res = "";
+    for(int i=1; i<n; i++){
+        if(s[i]!=s[i-1]){
+            // cout<<s[i]<<" "<<s[i-1]<<" "<<i<<endl;
+            res += s[i-1];
+        }
+    }
+    // cout<<s;
+
+    return res;
+}
+
