c9aa51abe23718455989052a4641390557779f90
Jason Lee <changjin9792@gmail.com>
2021-07-20 12:35:20 +0800

    Rotation Groups

---
+++ Leetcode/Array/Rotation Groups.cpp
@@ -0,0 +1,17 @@
+int solve(vector<string>& words) {
+    int n = words.size();
+    int res = 0;
+    unordered_set<string>st;
+
+    for(string &word: words){
+        if(st.count(word)) continue;
+        else{
+            for(int i=0; i<word.length(); i++){
+                st.insert(word.substr(i)+word.substr(0,i));
+            }
+            res++;
+        }
+    }
+
+    return res;
+}
