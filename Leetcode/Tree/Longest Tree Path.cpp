f9b8b384d990b4fbd94f0eca24157aebf3478e3e
Jason Lee <changjin9792@gmail.com>
2021-07-23 14:18:37 +0800

    Longest Tree Path

---
+++ Leetcode/Tree/Longest Tree Path.cpp
@@ -0,0 +1,15 @@
+int res;
+int help(Tree* root){
+    if(!root) return 0;
+    int left = help(root->left);
+    int right = help(root->right);
+    res = max(res,1+left+right);
+
+    return 1 + max(left,right);
+}
+
+int solve(Tree* root) {
+    res = 0;
+    help(root);
+    return res;
+}
