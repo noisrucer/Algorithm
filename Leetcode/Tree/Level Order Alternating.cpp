2d51803b78e5e9e2e4c46541b419e243ceeef7db
Jason Lee <changjin9792@gmail.com>
2021-07-23 11:56:22 +0800

    Level Order Alternating

---
+++ Leetcode/Tree/Level Order Alternating.cpp
@@ -0,0 +1,35 @@
+/**
+ * class Tree {
+ *     public:
+ *         int val;
+ *         Tree *left;
+ *         Tree *right;
+ * };
+ */
+vector<int> solve(Tree* root) {
+    deque<Tree*>dq;
+    dq.push_front(root);
+    int level = 0;
+    vector<int>res;
+
+    while(!dq.empty()){ 
+        int n = dq.size();
+        for(int i=0; i<n; i++){
+           if(level%2==0){
+               auto cur = dq.front(); dq.pop_front();
+               res.push_back(cur->val);
+               if(cur->left) dq.push_back(cur->left); 
+               if(cur->right) dq.push_back(cur->right); 
+           }else{
+               auto cur = dq.back(); dq.pop_back();
+               res.push_back(cur->val);
+               if(cur->right) dq.push_front(cur->right); 
+               if(cur->left) dq.push_front(cur->left); 
+           }
+        }
+        // for(auto i: res) cout<<i<<" ";
+        // cout<<endl;
+        level++;
+    }
+    return res;
+}
