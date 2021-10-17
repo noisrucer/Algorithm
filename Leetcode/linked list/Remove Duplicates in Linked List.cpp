7fcbb971a92d1792c175ee56192929354a6c50a8
Jason Lee <changjin9792@gmail.com>
2021-10-17 22:19:20 +0900

    Remove Duplicates in Linked List

---
+++ Leetcode/linked list/Remove Duplicates in Linked List.cpp
@@ -0,0 +1,21 @@
+LLNode* solve(LLNode* node) {
+    if(!node) return NULL;
+    unordered_set<int>st;
+    LLNode* cur = node;
+    LLNode* next = node;
+    st.insert(cur->val);
+
+    while(cur && cur->next){
+        next = cur->next;
+        if(st.count(next->val)){
+            cur->next = next->next;
+        }else{
+            cur = cur->next;
+        }
+        st.insert(next->val);
+    }
+
+
+    return node;
+    
+}
