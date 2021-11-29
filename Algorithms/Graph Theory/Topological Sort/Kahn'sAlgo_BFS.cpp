0c6db2d4cda776b4e5c80f27baf21070e2753152
Jason Lee <changjin9792@gmail.com>
2021-11-30 00:19:17 +0900

    Topological Sort - Kahn's Algorithm / BFS

--- Algorithms/Graph Theory/Topological Sort/Kahn'sAlgo_BFS.cpp
+++ Algorithms/Graph Theory/Topological Sort/Kahn'sAlgo_BFS.cpp
@@ -1,48 +1,40 @@
 class Solution {
 public:
-    vector<int>res;
-    void topo(vector<vector<int>>&adj, queue<int>Q, vector<int>indegree){
-        while(!Q.empty()){
-            auto cur = Q.front(); Q.pop();
-            res.push_back(cur);
-            for(auto &j: adj[cur]){
-                if(!--indegree[j]) Q.push(j);
-            }
-        }
-    }
-    
-    
-    bool isCycle(vector<vector<int>>&adj, queue<int>Q, vector<int>indegree){
-        int cycle_cnt = indegree.size();
-        while(!Q.empty()){
-            auto cur = Q.front(); Q.pop();
-            cycle_cnt--;
-            for(auto &j: adj[cur]){
-                if(!--indegree[j]) Q.push(j);
-            }
-        }
-        
-        return cycle_cnt;
-    }
-    
-    vector<int> findOrder(int v, vector<vector<int>>& pre) {
-        vector<vector<int>>adj(v);
-        vector<int>indegree(v,0);
+    vector<int> findOrder(int n, vector<vector<int>>& pre) {
+        vector<int>res; // Topologically Sorted Schedule
+        vector<vector<int>>adj(n); // Adj. List
+        vector<int>indegree(n, 0); // indegree
         queue<int>Q;
         
+        // Construct Adj. List & Indegree array
         for(auto &course: pre){
             adj[course[1]].push_back(course[0]);
             indegree[course[0]]++;
         }
         
-        for(int i=0; i<v; i++)
-            if(indegree[i]==0) Q.push(i);
+        // Push all 0-indegree nodes
+        for(int i=0; i<n; i++){
+            if(indegree[i] == 0) Q.push(i);
+        }
         
-        if(isCycle(adj,Q,indegree)) return {};
+        int cnt = 0;
         
-        topo(adj,Q,indegree);
+        // Recursively eliminate all the edges with BFS
+        while(!Q.empty()){
+            auto cur = Q.front(); Q.pop();
+            res.push_back(cur); // "IF" it's a DAG, this will be the answer
+            cnt++;
+            
+            for(auto &neighbor: adj[cur]){
+                if(--indegree[neighbor] == 0) Q.push(neighbor);
+            }
+        }
+        
+        // Check if the graph is cyclic
+        if(cnt != n) return {};
+        
+        // If not cyclic, we already found the topogical sort from above
         return res;
-        
         
     }
 };
