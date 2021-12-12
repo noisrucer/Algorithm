a705a1492cafca134349f477d1d1f1aecbc9711b
Jason Lee <changjin9792@gmail.com>
2021-12-13 00:09:05 +0900

    Longest SUbstring Without Repeating Characters

--- Leetcode/String/#3. Longest Substring Without Repeating Characters.cpp
+++ Leetcode/String/#3. Longest Substring Without Repeating Characters.cpp
@@ -1,3 +1,46 @@
+
+// 1. Extreme Optimizatio - Beats 100%
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int start = -1, max_len = 0;
+        vector<int>pos(256,-1);
+        
+        for(int i=0; i<s.length(); i++){
+           if(pos[s[i]] >= start) start = pos[s[i]] + 1;
+           pos[s[i]] = i; 
+            max_len = max(max_len, i-start+1);
+        }
+        
+        return max_len;
+    }
+};
+
+// 2. Optimized  using character vector 256
+class Solution {
+public:
+    int lengthOfLongestSubstring(string s) {
+        int n = s.length();
+        int start = 0, max_len = 0, cur_len = 0;
+        vector<int>alphabets(256,0);
+        
+        for(int i=0; i<n; i++){
+           while(alphabets[s[i]] > 0){
+               alphabets[s[start++]] = 0;
+               cur_len--;
+           }
+            
+            alphabets[s[i]]++;
+            cur_len++;
+    
+            if(cur_len > max_len) max_len = cur_len;
+        }
+        
+        return max_len;
+    }
+};
+
+// 3. Sliding Window using unordered_set
 class Solution {
 public:
     int lengthOfLongestSubstring(string s) {
