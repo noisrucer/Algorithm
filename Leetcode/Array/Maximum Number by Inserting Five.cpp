af81ca090915a5e913c0cbb9ea52ff1f2989a8d4
Jason Lee <changjin9792@gmail.com>
2021-12-16 23:58:40 +0900

    Maximum number by inserting five

---
+++ Leetcode/Array/Maximum Number by Inserting Five.cpp
@@ -0,0 +1,21 @@
+int solve(int n) {
+    if(n==0) return 50;
+    int isPositive = (n >= 0);
+    string num = to_string(abs(n));
+    int len = num.size();
+    int i;
+
+    for(i = 0; i<len; i++){
+        if(isPositive){
+            if(num[i]-'0' < 5) break;
+        }else{
+            if(num[i]-'0' > 5) break;
+        }
+    }
+
+    num.insert(i,"5");
+    if(!isPositive) num = "-" + num;
+    return stoi(num);
+
+    
+}
