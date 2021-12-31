f718a8f589e648449dbf0c2b33f87674c2ac1cf7
Jason Lee <changjin9792@gmail.com>
2021-12-31 19:36:17 +0900

    eidt

--- 알고리즘수업/DP/#509. Fibonacci Number.py
+++ 알고리즘수업/DP/#509. Fibonacci Number.py
@@ -3,11 +3,14 @@
 2) 알고리즘: Dynamic Programming / Recursion
 3) 자료구조: Array for DP
 4) 해결전략: 
-   1) Generalization
+   1) Generalization - fibo(n) = nth fibonacci number
    2) Define the "Recurrence Relation" -> [IMPORTANT] 1) 과정을 "그대로" 점화식으로 변경.
       - General Case / Base Case
       - Hardest step
+      - f(n) = nth fibonacci number
+      - f(n) = f(n-1) + f(n-2)
    3) Define Topdown function -> [IMPORTANT]점화식을 "그대로" return함
+      - int topdown(int n) -> nth fibonacci number을 리턴함
    4) Memoization(DP Step)
    5) DONE!
 
