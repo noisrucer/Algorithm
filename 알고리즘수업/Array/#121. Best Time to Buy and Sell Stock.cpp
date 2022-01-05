853ca8f962888e659f27467ca0805f90d426a80b
Jason Lee <changjin9792@gmail.com>
2022-01-05 12:57:59 +0900

    Best time to buy and sell stock

---
+++ 알고리즘수업/Array/#121. Best Time to Buy and Sell Stock.cpp
@@ -0,0 +1,15 @@
+class Solution:
+    def maxProfit(self, prices: List[int]) -> int:
+        # Q) Why is this question considered as a "Maximum Subarray" question?
+        n = len(prices)
+        
+        # cur_max, glob_max initialization
+        glob_max = cur_max = -1
+        
+        for i in range(1, n):
+            diff = prices[i] - prices[i-1]
+            cur_max = max(diff, cur_max + diff)
+            glob_max = max(glob_max, cur_max)
+        
+        return glob_max if glob_max is not -1 else 0
+            
